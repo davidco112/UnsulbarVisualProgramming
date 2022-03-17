b64775ca20bca821b9670d0c1b094e400ea6652e
davidco112 <62873901+davidco112@users.noreply.github.com>
2022-03-17 14:56:31 +0800

    Update David Yonatan_praktek objek.py

--- David Yonatan_praktek objek.py
+++ David Yonatan_praktek objek.py
@@ -8,14 +8,17 @@ eudora = Hero()
 #att = attack
 #arm = armour
 
+tigreal.name = 'tigreal'
 tigreal.hp = '100'
 tigreal.att = '10'
 tigreal.arm = '40';
 
+lesley.name = 'lesley'
 lesley.hp = '100'
 lesley.att = '50'
 lesley.arm = '20'
 
+eudora.name = 'eudora'
 eudora.hp = '100'
 eudora.att = '30'
 eudora.arm = '5'
